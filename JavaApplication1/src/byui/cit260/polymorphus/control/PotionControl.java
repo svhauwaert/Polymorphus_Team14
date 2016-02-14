57cc3c032ac11b52f426dc19ad4515523f07411f
swalters <swalters@SUPERMAN.spencer.local>
2016-02-13 18:57:25 -0800

    Spencer Walters created PotionControl

--- JavaApplication1/src/byui/cit260/polymorphus/control/PotionControl.java
+++ JavaApplication1/src/byui/cit260/polymorphus/control/PotionControl.java
@@ -11,4 +11,19 @@ package byui.cit260.polymorphus.control;
  */
 public class PotionControl {
     
+    public double concoctItem(double requireItem, double item, double amount){
+    
+     if (requireItem != 3) { // Items are not three
+          return -1;
+    }
+     if (item != 2) { // two of each item
+         return -1;
+     }
+     if (amount <=4 ) { //need 4 of each
+          return -1;
+     }
+     double mixItem = requireItem + (1 + (item * amount)); // Round to a whole number
+     return mixItem;
+    }
 }
+
